b70f4da259d274dc99b181d29bf2122033c419b6
madou9 <issoufou.mamadou@yahoo.com>
2025-08-20 16:53:09 +0200

    defined my function that i need for the polynomial

--- main.go
+++ main.go
@@ -1,5 +1,37 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
-	println("Hello, World!")
+	if (len(os.Args) < 2) {
+		fmt.Println("Please check your argument")
+	}
+	equation := os.Args[1]
+	fmt.Println("Input equation: ", equation)
+
+	parseEquation(equation)
 }
+
+func parseEquation(eq string) {
+	fmt.Println("parse equation: ", eq)
+}
+
+func reduceEquation(){
+	fmt.Println("reduce equation")
+}
+
+func getDegree(){
+	fmt.Println("get equation")
+}
+
+func solveEquation(){
+	fmt.Println("solve equation")
+}
+
+func printResult(){
+	fmt.Println("print Result")
+}
+
